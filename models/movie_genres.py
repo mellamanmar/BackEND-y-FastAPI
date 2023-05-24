a73f1a06950edf08156f20bca3558afe8c458d5a
mellamanmar <marimelissamc@gmail.com>
2023-05-24 10:52:46 -0500

    Create models genres and movie genres

---
+++ models/movie_genres.py
@@ -0,0 +1,13 @@
+from aqlalchemy import Column, ForeingKey, Integer
+
+from config import Base
+
+class MovieGenres(Base):
+
+    __tablename__= "movie_genres"
+
+    id= Column (Integer, primary_key=True)
+    gen_id= Column (Integer, ForeingKey("genre.id"))
+    movie_id= Column (Integer, ForeingKey("movie.id"))
+
+    
