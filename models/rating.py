7fde7cc45b2b8ad4b4de1e3071135f6dcbeb753c
JMCardenass <jmcardenas1807@gmail.com>
2023-05-24 15:51:37 -0500

    complete the rating table

---
+++ models/rating.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Column, ForeignKey , Integer
+
+from config.database import Base
+
+class Rating(Base):
+    __tablename__ = "rating"
+
+    id = Column(Integer, primary_key=True)
+    movie_id = Column(Integer, ForeignKey("movie.id"))
+    rev_id = Column(Integer, ForeignKey("user.id"))
+    rev_stars = Column(Integer)
+    num_o_ratings = Column(Integer)
