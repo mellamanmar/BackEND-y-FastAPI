c00cb7f6d78c069b4ebd900b0afc64be6aa13e3d
mellamanmar <marimelissamc@gmail.com>
2023-05-24 15:28:29 -0500

    Update

--- models/movie_genres.py
+++ models/movie_genres.py
@@ -1,17 +1,4 @@
-<<<<<<< HEAD
 from sqlalchemy import Column, ForeignKey, Integer
-
-from config import Base 
-
-class MovieGenres (Base):
-    
-    __tablename__ ="movie_genres"
-    
-    id = Column(Integer,primary_key=True)
-    gen_id = Column(Integer,ForeignKey("genres.id"))
-    movie_id = Column(Integer,ForeignKey("movie.id"))
-=======
-from sqlalchemy import Column, ForeingKey, Integer
 
 from config.database import Base
 
@@ -20,8 +7,7 @@ class MovieGenres(Base):
     __tablename__= "movie_genres"
 
     id= Column (Integer, primary_key=True)
-    gen_id= Column (Integer, ForeingKey("genre.id"))
-    movie_id= Column (Integer, ForeingKey("movie.id"))
+    gen_id= Column (Integer, ForeignKey("genre.id"))
+    movie_id= Column (Integer, FForeignKey("movie.id"))
 
     
->>>>>>> main
