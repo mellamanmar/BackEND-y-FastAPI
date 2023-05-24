3fc5758cb25199461fb09ae07be29ff4de7b1ca2
johansebastianbeltran <sebastianrincon834@gmail.com>
2023-05-24 14:47:26 -0500

    problem solution

--- models/genres.py
+++ models/genres.py
@@ -2,17 +2,8 @@ from sqlalchemy import Column, Integer, String
 
 from config.database import Base
 
-<<<<<<< HEAD
-class Genres(Base):
-    
-    __tablename__ ="genres"
-    
-    id = Column(Integer,primary_key=True)
-    gen_title = Column(String)
-=======
 class Genres (Base):
     __tablename__ = "genres"
 
     id= Column(Integer, primary_key=True)
     gen_title= Column(String)
->>>>>>> main
--- models/movie_genres.py
+++ models/movie_genres.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from sqlalchemy import Column, ForeignKey, Integer
 
 from config import Base 
@@ -10,18 +9,3 @@ class MovieGenres (Base):
     id = Column(Integer,primary_key=True)
     gen_id = Column(Integer,ForeignKey("genres.id"))
     movie_id = Column(Integer,ForeignKey("movie.id"))
-=======
-from sqlalchemy import Column, ForeingKey, Integer
-
-from config.database import Base
-
-class MovieGenres(Base):
-
-    __tablename__= "movie_genres"
-
-    id= Column (Integer, primary_key=True)
-    gen_id= Column (Integer, ForeingKey("genre.id"))
-    movie_id= Column (Integer, ForeingKey("movie.id"))
-
-    
->>>>>>> main
