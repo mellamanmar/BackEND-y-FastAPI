e31ce689997f4f334b60838a446b9b45c3f4ac05
JMCardenass <jmcardenas1807@gmail.com>
2023-05-24 15:11:43 -0500

    Add columns of movie genre

--- models/genres.py
+++ models/genres.py
@@ -1,18 +1,9 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, String, Integer
 
 from config.database import Base
 
-<<<<<<< HEAD
-class Genres(Base):
-    
-    __tablename__ ="genres"
-    
-    id = Column(Integer,primary_key=True)
-    gen_title = Column(String)
-=======
-class Genres (Base):
-    __tablename__ = "genres"
+class genre (Base):
+    __tablename__ = "genre"
 
-    id= Column(Integer, primary_key=True)
-    gen_title= Column(String)
->>>>>>> main
+    id = Column(Integer, primary_key = True)
+    gen_ttile= Column(String)
