eb79afee8ad07346cd7343ed19687e866c487bd3
johansebastianbeltran <sebastianrincon834@gmail.com>
2023-05-29 16:43:33 -0500

    Aceptacion de cambios main

--- routers/genres.py
+++ routers/genres.py
@@ -3,7 +3,7 @@ from fastapi.responses import HTMLResponse, JSONResponse
 from service.genres import GenresService
 from config.database import Session
 from fastapi.encoders import jsonable_encoder
-from schemas.genres import Genres as GenresSchema
+from schemas.genres import Genres
 
 genres_router= APIRouter()
 
@@ -28,11 +28,28 @@ def get_genres_fot_id(id:int):
 
 
 @genres_router.post ('/genres', tags= ['genres'], status_code=201)
-def create_genres(genres: GenresSchema):
+def create_genres(genres: Genres):
     #llamar a una función que va a estar en el servicio
     db= Session()
     GenresService(db).create_genre(genres)
     return JSONResponse (content={"menssage": "genre created succesfully", "status_code": 201}, status_code= 201)
-
 #para el genres delete debemos verificar que el id existe y después ese género lo eliminamos
 # creamos un get que tare un sólo género por id (para crear las acciones put y delete)
+
+@genres_router.put('/genres{id}',tags=['genres'])
+def update_genre(id:int,data:Genres):
+    db = Session()
+    result = GenresService(db).get_for_id(id)
+    if not result:
+        return JSONResponse(content={'message':"genre don't gound","status_code":404})
+    GenresService(db).update_genre(data)
+    return JSONResponse (content={"menssage": "genre created succesfully", "status_code": 202}, status_code= 202)
+
+@genres_router.delete('/genres{id}',tags=['genres'])
+def delete_genre(id:int):
+    db = Session()
+    result = GenresService(db).get_for_id(id)
+    if not result:
+        return JSONResponse(content={'message':"genre don't gound","status_code":404})
+    GenresService(db).delete_genre(id)
+    return JSONResponse (content={"menssage": "genre delete succesfully", "status_code": 200}, status_code= 200)
--- service/genres.py
+++ service/genres.py
@@ -1,5 +1,5 @@
 from models.genres import Genres as GenresModel
-
+from schemas.genres import Genres
 class GenresService():
     def __init__(self, db):
         self.db = db
@@ -19,3 +19,14 @@ class GenresService():
     def get_for_id (self, id:int):
         result= self.db.query(GenresModel).filter(GenresModel.id == id).first()
         return result
+    
+    def update_genre(self,data:Genres):
+        genre = self.db.query(GenresModel).filter(GenresModel.id == data.id).first()
+        genre.gen_title = data.gen_title
+        self.db.commit()
+        return
+    
+    def delete_genre(self,id:int):
+        self.db.query(GenresModel).filter(GenresModel.id == id).delete()
+        self.db.commit()
+        
