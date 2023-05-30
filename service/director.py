f42427f05fbf8dfccdc426476b4a0855df7a17ed
KevinSerna <kevinsernap@gmail.com>
2023-05-30 17:31:30 -0500

    Create director in service and routers

---
+++ service/director.py
@@ -0,0 +1,34 @@
+from models.director import Director as DirectorModel
+from schemas.director import Director as DirectorSchema
+
+class DirectorService():
+    def __init__(self, db):
+        self.db = db
+    
+    def get_director(self):
+        result = self.db.query(DirectorModel).all()
+        return result
+
+    def create_director(self, director: DirectorModel):
+        new_director = DirectorModel(
+            dir_fname = director.dir_fname.upper(),
+            dir_lname = director.dir_lname.upper()
+        )
+        self.db.add(new_director)
+        self.db.commit()
+        return
+    
+    def get_director_by_id(self, id: int):
+        result = self.db.query(DirectorModel).filter(DirectorModel.dir_id == id).first()
+        return result
+
+    def update_director(self, data: DirectorModel):
+        director = self.db.query(DirectorModel).filter(DirectorModel.dir_id == data.dir_id).first()
+        director.dir_fname = data.dir_fname
+        director.dir_lname = data.dir_lname
+        self.db.commit()
+        return 
+
+    def delete_director(self, id: int):
+        self.db.query(DirectorModel).filter(DirectorModel.dir_id == id).delete()
+        self.db.commit()
