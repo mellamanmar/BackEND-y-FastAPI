b8f212921845baf9016a858a210ba47f337c15ed
KevinSerna <kevinsernap@gmail.com>
2023-05-31 14:36:51 -0500

    change in director

--- service/director.py
+++ service/director.py
@@ -1,34 +1,35 @@
 from models.director import Director as DirectorModel
-from schemas.director import Director as DirectorSchema
-
+from schemas.director import Director
 class DirectorService():
-    def __init__(self, db):
+    def init(self, db):
         self.db = db
-    
+        
     def get_director(self):
         result = self.db.query(DirectorModel).all()
         return result
-
-    def create_director(self, director: DirectorModel):
+    
+    def  create_director(self,director:DirectorModel):
         new_director = DirectorModel(
             dir_fname = director.dir_fname.upper(),
-            dir_lname = director.dir_lname.upper()
+            dir_iname = director.dir_iname.upper(),
+            
         )
         self.db.add(new_director)
         self.db.commit()
         return
     
-    def get_director_by_id(self, id: int):
-        result = self.db.query(DirectorModel).filter(DirectorModel.dir_id == id).first()
+    def get_for_id(self,id:int):
+        result = self.db.query(DirectorModel).filter(DirectorModel.id == id). first()
         return result
-
-    def update_director(self, data: DirectorModel):
-        director = self.db.query(DirectorModel).filter(DirectorModel.dir_id == data.dir_id).first()
+    
+    def update_director(self,data:Director):
+        director = self.db.query(DirectorModel).filter(DirectorModel.id ==data.id).first()
         director.dir_fname = data.dir_fname
-        director.dir_lname = data.dir_lname
+        director.dir_iname = data.dir_iname
         self.db.commit()
-        return 
-
-    def delete_director(self, id: int):
-        self.db.query(DirectorModel).filter(DirectorModel.dir_id == id).delete()
+        return
+    
+    def delete_director(self,id:int):
+        self.db.query(DirectorModel).filter(DirectorModel.id == id).delete()
         self.db.commit()
+        return
